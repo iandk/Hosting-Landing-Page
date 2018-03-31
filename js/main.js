bc6015dfbbeea0e6b3129a06127b88c64dfd5ad2
v3ng <iandavid2000@googlemail.com>
2018-03-31 23:59:55 +0200

    Adding files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("#button-contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+});
+
+$("#button-pricing").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pricing").offset().top
+    }, 1000);
+});
